26102cee5ab86fc2d90d3877f42de350013ee6b1
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-06 23:59:17 -0300

    config style component card game

--- _matching_game/main.js
+++ _matching_game/main.js
@@ -1,3 +1,7 @@
+import './src/styles/settings/colors.css';
+import './src/styles/generic/reset.css';
+import './src/styles/elements/base.css';
+
 import CardGame from "./src/components/CardGame";
 
 const $root = document.querySelector( '#root' );
--- _matching_game/src/components/CardGame/index.js
+++ _matching_game/src/components/CardGame/index.js
@@ -1,8 +1,9 @@
-// jsx
+import './style.css'; // por termos construido o app com o vite, conseguimos usar esse mecanismo
+
 const CardGame = () => {
-    return `
-        <article>
-            CardGame
+    return /* html */`
+        <article class='card-game'>
+            <img src='images/alura_pixel.png' alt="Logo do Card Game">
         </article>
     `
     ;
