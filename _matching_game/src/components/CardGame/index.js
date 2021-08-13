5fa81a5fa0d3002c7dfedefaebf9c94f4923d895
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-13 13:34:49 -0300

    alter function construct CardGame add default paramather images path

--- _matching_game/src/components/CardGame/index.js
+++ _matching_game/src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css'; // por termos construido o app com o vite, conseguimos usar esse mecanismo
 
-const CardGame = () => {
+const CardGame = ( icon = 'alura_pixel' , alt = 'Logo da Alura' ) => {
     return /* html */`
         <article class='card-game'>
-            <img src='images/alura_pixel.png' alt="Logo do Card Game">
+            <img src='images/${ icon }.png' alt="${ alt }">
         </article>
     `
     ;
