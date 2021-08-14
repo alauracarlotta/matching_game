19549c6a3490ae9119104f73e8fe2abc49df4606
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-14 00:51:56 -0300

    wip: create component vs score without css

--- _matching_game/src/objects/Score/index.js
+++ _matching_game/src/objects/Score/index.js
@@ -1,11 +1,13 @@
 import './style.css';
 
 import PlayerName from '../../components/PlayerName';
+import VsPlayer from '../../components/VsPlayer';
 
 const Score = () => {
 	return /* html */ `
 		<header class='scoreboard'>
 			${ PlayerName( 'Player1' ) }
+			${ VsPlayer() } 
 			${ PlayerName( 'Player2' ) }
 		</header>
 	`;
